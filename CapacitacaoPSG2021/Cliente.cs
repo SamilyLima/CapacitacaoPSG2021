5d2acf572cfb4191ee03c6b03cebae7f67162353
Samily Lima <66699126+SamilyLima@users.noreply.github.com>
2021-07-15 22:15:36 -0400

    Add files via upload

---
+++ CapacitacaoPSG2021/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapacitacaoPSG2021
+{
+    public abstract class Cliente
+    {
+        private int codCliente;
+
+        private DateTime dataInclusao;
+
+        public int CodCliente
+        {
+            get { return this.codCliente; }
+            set { this.codCliente = value; }
+        }
+
+        public DateTime DataInclusao
+        {
+            get { return this.dataInclusao; }
+            set { this.dataInclusao = value; }
+        }
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(int codigoCliente, DateTime dataInclusao)
+        {
+            this.codCliente = codigoCliente;
+            this.dataInclusao = dataInclusao;
+        }
+    }
+}
